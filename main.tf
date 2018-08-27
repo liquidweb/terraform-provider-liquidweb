3395845a672368a45abf36d8e172f646b2758809
caiges <caigesn@gmail.com>
2018-08-27 10:56:48 -0700

    Use a variable for the storm server password

--- main.tf
+++ main.tf
@@ -2,15 +2,19 @@ variable "storm_config_path" {
   type = "string"
 }
 
+variable "api_server_password" {
+  type = "string"
+}
+
 provider "storm" {
   config_path = "${var.storm_config_path}"
 }
 
 resource "storm_server" "api_servers" {
   config_id      = 1090
-  template       = "UBUNTU_1804_UNMANAGED"   // ubuntu 18.04
+  template       = "UBUNTU_1804_UNMANAGED"      // ubuntu 18.04
   domain         = "terraform.dev"
-  password       = "11111aA"
+  password       = "${var.api_server_password}"
   public_ssh_key = "${file("./devkey.pub")}"
   zone           = 12
 }
