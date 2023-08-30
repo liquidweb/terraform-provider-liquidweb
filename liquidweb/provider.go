214d0239ba33ed17ee4c4023b2de04e42f23cb92
Jack Hayhurst <jhayhurst@liquidweb.com>
2023-08-30 11:49:16 -0400

    reworked asset names

--- liquidweb/provider.go
+++ liquidweb/provider.go
@@ -16,14 +16,21 @@ func Provider() *schema.Provider {
 		},
 		DataSourcesMap: map[string]*schema.Resource{
 			"liquidweb_network_zone":        dataSourceLWNetworkZone(),
+			"liquidweb_cloud_server_config": dataSourceLWStormServerConfig(),
+
+			// backwards compat
 			"liquidweb_storm_server_config": dataSourceLWStormServerConfig(),
 		},
 		ResourcesMap: map[string]*schema.Resource{
-			"liquidweb_storage_block_volume":  resourceStorageBlockVolume(),
-			"liquidweb_storm_server":          resourceStormServer(),
+			"liquidweb_cloud_block_storage":   resourceStorageBlockVolume(),
+			"liquidweb_cloud_server":          resourceStormServer(),
 			"liquidweb_network_dns_record":    resourceNetworkDNSRecord(),
 			"liquidweb_network_load_balancer": resourceNetworkLoadBalancer(),
 			"liquidweb_network_vip":           resourceNetworkVIP(),
+
+			// backwards compat
+			"liquidweb_storage_block_volume": resourceStorageBlockVolume(),
+			"liquidweb_storm_server":         resourceStormServer(),
 		},
 		ConfigureFunc: providerConfigure,
 	}
