828f57beafbf5329bcb223df02d282da5170f0d2
Jack Hayhurst <jhayhurst@liquidweb.com>
2023-09-07 14:04:54 -0400

    removed all config file reading junk

--- liquidweb/config.go
+++ liquidweb/config.go
@@ -1,15 +1,11 @@
 package liquidweb
 
 import (
-	"errors"
 	"fmt"
 	"log"
 	"os"
-	"path/filepath"
 	"strings"
 
-	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
-	"github.com/kkyr/fig"
 	lwgoapi "github.com/liquidweb/liquidweb-go/client"
 )
 
@@ -44,61 +40,32 @@ func NewConfig(username string, password string, url string, timeout int, nClien
 }
 
 // GetConfig handles mapping configuration between Terraform & Liquid Web's Liquidweb API configuration.
-func GetConfig(resourceData *schema.ResourceData) (interface{}, error) {
-	configPaths := []string{
-		"./",
-		"/",
-	}
-	if homedir, err := os.UserHomeDir(); err != nil {
-		configPaths = append(configPaths, homedir)
-	}
-
-	configName := "lwapi.toml"
-
-	if optConfigFile, isSet := resourceData.GetOk("config_path"); isSet {
-		if val, ok := optConfigFile.(string); ok {
-			log.Printf("included config file: %s\n", val)
-			configPaths = append([]string{filepath.Dir(val)}, configPaths...)
-			configName = filepath.Base(val)
-		}
-	}
-
-	var cfg struct {
-		lwApiCfg struct {
-			Username string `fig:"username"`
-			Password string `fig:"password"`
-		} `fig:"lwapi"`
-	}
-
-	if err := fig.Load(&cfg,
-		fig.File(configName),
-		fig.Dirs(configPaths...),
-	); err != nil && !errors.Is(err, fig.ErrFileNotFound) {
-		return nil, err
-	}
+func GetConfig() (interface{}, error) {
+	var username, token string
 
 	for _, e := range os.Environ() {
 		pair := strings.SplitN(e, "=", 2)
 		switch pair[0] {
 		case "LWAPI_USERNAME":
-			cfg.lwApiCfg.Username = pair[1]
+			log.Printf("read username %s\n", pair[1])
+			username = pair[1]
 		case "LWAPI_PASSWORD":
-			cfg.lwApiCfg.Password = pair[1]
+			token = pair[1]
 		}
 	}
 
-	if cfg.lwApiCfg.Username == "" {
+	if username == "" {
 		return nil, ErrUsernameNotSet
 	}
 
-	if cfg.lwApiCfg.Password == "" {
+	if token == "" {
 		return nil, ErrSecretNotSet
 	}
 
 	// Initial new LW go client.
 	lwAPI, err := lwgoapi.NewAPI(
-		cfg.lwApiCfg.Username,
-		cfg.lwApiCfg.Password,
+		username,
+		token,
 		lwApiURL,
 		lwApiTimeout)
 	if err != nil {
@@ -106,8 +73,8 @@ func GetConfig(resourceData *schema.ResourceData) (interface{}, error) {
 	}
 
 	return NewConfig(
-		cfg.lwApiCfg.Username,
-		cfg.lwApiCfg.Password,
+		username,
+		token,
 		lwApiURL,
 		lwApiTimeout,
 		lwAPI,
